145f9a8c762f2ae31ac9e3fec85a3bc3214e7d81
NarenCandy <narencrazy007@gmail.com>
2026-01-31 00:06:29 +0530

    removed error

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,7 +42,7 @@ const frontendPath = path.join(__dirname, "../../frontend/dist");
 
 app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
+app.get("/{*any}", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
